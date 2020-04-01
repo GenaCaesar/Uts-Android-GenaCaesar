fc607b32e9ed29d240f42dbdce6a7620a16bb9a4
Gena Caesar Athallah <genac75@gmail.com>
2020-04-01 20:30:05 +0700

    Menambah Button Link yang Terhubung pada Website Drakor (https://ratudrama.id/)

--- app/src/main/java/com/example/drakorsyn/activities/DetailActivity.java
+++ app/src/main/java/com/example/drakorsyn/activities/DetailActivity.java
@@ -1,15 +1,22 @@
 package com.example.drakorsyn.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.bumptech.glide.Glide;
 import com.example.drakorsyn.R;
 import com.example.drakorsyn.models.Drakor;
 
 public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_DRAKOR = "extra_drakor";
+    private Button btnLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,18 @@ public class DetailActivity extends AppCompatActivity {
         TextView deskripsi = findViewById(R.id.tv_desc_detail);
         TextView genre = findViewById(R.id.tv_genre_detail);
         TextView pemeran = findViewById(R.id.tv_pemeran_detail);
+        btnLink = (Button)findViewById(R.id.btnLink);
+
+        btnLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://ratudrama.id/"));
+                startActivity(intent);
+            }
+        });
 
         Drakor drakor = getIntent().getParcelableExtra(EXTRA_DRAKOR);
 
@@ -29,5 +48,5 @@ public class DetailActivity extends AppCompatActivity {
         deskripsi.setText(drakor.getDeskripsi());
         genre.setText(drakor.getGenre());
         pemeran.setText(drakor.getPemeran());
-    };
+    }
 }
